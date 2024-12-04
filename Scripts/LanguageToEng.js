607557dfe646d9f570795d31b4f6da47bf863b67
Mooncakovich <90803917+Mooncakovich@users.noreply.github.com>
2024-12-04 15:22:41 +0300

    Add files via upload

---
+++ Scripts/LanguageToEng.js
@@ -0,0 +1,14 @@
+document.getElementById('toggleButtonRus').addEventListener('click', function (event) {
+        event.preventDefault(); 
+        const rus = document.getElementById('rus');
+        const eng = document.getElementById('eng');
+    
+        // Переключаем видимость блоков
+        if (rus.style.display === 'none') {
+            rus.style.display = 'block';
+            eng.style.display = 'none';
+        } else {
+            rus.style.display = 'none';
+            eng.style.display = 'block';
+        }
+    });
